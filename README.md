fa04a4075693efa19d8a93550ee32359489c33f2
Frenzoid <frenzoid@protonmail.com>
2021-07-12 15:48:25 +0200

    final commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Decentragram
+
+## 🔧 Project Diagram:
+![Project Diagram](https://i.gyazo.com/e7fa5d05ef7806419b4897ecc668a045.png)
---
+++ src/components/Navbar.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import Identicon from 'identicon.js';
+import photo from '../photo.png'
+
+class Navbar extends Component {
+
+  render() {
+    return (
+      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <img src={photo} width="30" height="30" className="d-inline-block align-top" alt="" />
+        Decentragram
+        <ul className="navbar-nav px-3">
+          <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+            <small className="text-secondary">
+              <small id="account">{this.props.account}</small>
+            </small>
+            {this.props.account
+              ? <img
+                className='ml-2'
+                width='30'
+                height='30'
+                src={`data:image/png;base64,${new Identicon(this.props.account, 30).toString()}`}
+              />
+              : <span></span>
+            }
+          </li>
+        </ul>
+      </nav >
+    );
+  }
+}
+
+export default Navbar;
